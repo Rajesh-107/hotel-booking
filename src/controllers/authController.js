5e3c6fd5e3e1b46ef360011d7594c8ceca5b6032
Rajesh-107 <applerb326@gmail.com>
2022-11-24 21:33:13 +0600

    hash password

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,17 @@
 const Users = require("../models/User");
-
+const bcrypt = require('bcryptjs');
 exports.register = async (req, res, next) => {
   try {
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
     const newUser = new Users({
       userName: req.body.userName,
       email: req.body.email,
-      password: req.body.password,
-    })
+      password: hash,
+    });
 
-    await newUser.save()
-    res.status(200).send("User has been created")
+    await newUser.save();
+    res.status(200).send("User has been created");
   } catch (err) {
     next(err);
   }
