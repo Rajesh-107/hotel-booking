3defa920af7c05605abf10194d844ec8f123da1c
Rajesh-107 <applerb326@gmail.com>
2022-11-24 21:59:40 +0600

    hash password check

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,8 @@
 const Users = require("../models/User");
 const bcrypt = require('bcryptjs');
+const { createError } = require("../utils/error");
+
+
 exports.register = async (req, res, next) => {
   try {
     const salt = bcrypt.genSaltSync(10);
@@ -12,6 +15,22 @@ exports.register = async (req, res, next) => {
 
     await newUser.save();
     res.status(200).send("User has been created");
+  } catch (err) {
+    next(err);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+   const user = await Users.findOne({userName:req.body.userName})
+   if(!user) return next(createError(404, "user not found!"))
+
+   const isPasswordCorrect = await bcrypt.compare(req.body.password, user.password)
+   if(!isPasswordCorrect) return next(createError(400, "wrong password!"))
+
+   const {password, isAdmin, ...otherDetails} = user._doc;
+
+    res.status(200).json({...otherDetails});
   } catch (err) {
     next(err);
   }
